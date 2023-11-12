ddedcea47fb5c44c04146a03aee530d15c4078a9
Yosuke Motosugi <y.motosugi0227@gmail.com>
2023-11-13 00:23:50 +1100

    Complete all iterations

---
+++ lib/event_manager.rb
@@ -0,0 +1,67 @@
+require 'csv'
+require 'google/apis/civicinfo_v2'
+require 'erb'
+
+template_letter = File.read('form_letter.erb')
+erb_template = ERB.new template_letter
+
+def clean_zip_code(zipcode)
+  zipcode.to_s.rjust(5, '0')[0..4]
+  # nil.to_s -> ''
+
+  # if zipcode.nil?
+  #   '00000'
+  # elsif zipcode.length < 5
+  #   zipcode.rjust(5, '0')
+  # elsif zipcode.length > 5
+  #   zipcode[0..4]
+  # else
+  #   zipcode
+  # end
+end
+
+def legislators_by_zipcode(zip)
+  civic_info = Google::Apis::CivicinfoV2::CivicInfoService.new
+  civic_info.key = 'AIzaSyClRzDqDh5MsXwnCWi0kOiiBivP6JsSyBw'
+
+  begin
+    civic_info.representative_info_by_address(
+      address: zip,
+      levels: 'country',
+      roles: ['legislatorUpperBody', 'legislatorLowerBody']
+    ).officials
+  rescue
+    'You can find your representatives by visiting www.commoncause.org/take-action/find-elected-officials'
+  end
+end
+
+def save_thank_you_letter(id, form_letter)
+  Dir.mkdir('output') unless Dir.exist?('output')
+
+  filename = "output/thanks_#{id}.html"
+
+  File.open(filename, 'w') do |file|
+    file.puts form_letter
+  end
+end
+
+puts 'Event Manager Initialized!'
+
+contents = CSV.open(
+  'event_attendees.csv',
+  headers: true,
+  header_converters: :symbol
+  )
+
+contents.each do |row|
+  id = row[0] # Added
+  name = row[:first_name]
+
+  zipcode = clean_zip_code(row[:zipcode])
+
+  legislators = legislators_by_zipcode(zipcode)
+
+  form_letter = erb_template.result(binding)
+
+  save_thank_you_letter(id, form_letter)
+end
